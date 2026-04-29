d4fa83d5d4353456aaa814cee78be2d9ace25add
Dylan Moreland <dylan@dimo.zone>
2026-04-29 15:22:03 -0400

    More forgiving

--- clickhouse/migrations/00008_add_voids_id_migration.sql
+++ clickhouse/migrations/00008_add_voids_id_migration.sql
@@ -1,17 +1,17 @@
 -- +goose Up
 -- +goose StatementBegin
-ALTER TABLE cloud_event ADD COLUMN voids_id String DEFAULT '' COMMENT 'For dimo.tombstone events, the id of the event being tombstoned. Empty for all other event types.' AFTER data_index_key;
+ALTER TABLE cloud_event ADD COLUMN IF NOT EXISTS voids_id String DEFAULT '' COMMENT 'For dimo.tombstone events, the id of the event being tombstoned. Empty for all other event types.' AFTER data_index_key;
 -- +goose StatementEnd
 -- +goose StatementBegin
 -- Make it cheap to find these events. This may also help with some attestation filtering.
 -- Need to materialize this on your own.
-ALTER TABLE cloud_event ADD INDEX idx_event_type event_type TYPE set(0) GRANULARITY 1;
+ALTER TABLE cloud_event ADD INDEX IF NOT EXISTS idx_event_type event_type TYPE set(0) GRANULARITY 1;
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-ALTER TABLE cloud_event DROP INDEX idx_event_type;
+ALTER TABLE cloud_event DROP INDEX IF EXISTS idx_event_type;
 -- +goose StatementEnd
 -- +goose StatementBegin
-ALTER TABLE cloud_event DROP COLUMN voids_id;
+ALTER TABLE cloud_event DROP COLUMN IF EXISTS voids_id;
 -- +goose StatementEnd
